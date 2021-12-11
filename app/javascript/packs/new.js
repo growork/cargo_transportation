31fc26a108dd2e1d664a314a014b0ba7a5d7fa08
growork <growork@mail.ru>
2021-12-12 02:35:21 +0300

    Добавлена карта для выбора маршрута доставки.

---
+++ app/javascript/packs/new.js
@@ -0,0 +1,54 @@
+ymaps.ready(init);
+
+function init() {
+    myMap = new ymaps.Map('map', {
+        center: [60.906882, 30.067233],
+        zoom: 9,
+        controls: []
+    }),
+        // Создадим панель маршрутизации.
+        routePanelControl = new ymaps.control.RoutePanel({
+            options: {
+                // Добавим заголовок панели.
+                showHeader: true,
+                title: 'Расчёт доставки'
+            }
+        }),
+        zoomControl = new ymaps.control.ZoomControl({
+            options: {
+                size: 'small',
+                float: 'none',
+                position: {
+                    bottom: 145,
+                    right: 10
+                }
+            }
+        });
+    // Пользователь сможет построить только автомобильный маршрут.
+    routePanelControl.routePanel.options.set({
+        types: {auto: true}
+    });
+
+    myMap.controls.add(routePanelControl).add(zoomControl);
+
+    // Получим ссылку на маршрут.
+    routePanelControl.routePanel.getRouteAsync().then(function (route) {
+
+        // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
+        route.model.setParams({results: 1}, true);
+
+        // Повесим обработчик на событие построения маршрута.
+        route.model.events.add('requestsuccess', function () {
+
+            var activeRoute = route.getActiveRoute();
+            if (activeRoute) {
+                // Получим протяженность маршрута.
+                var length = route.getActiveRoute().properties.get("distance");
+
+                document.getElementById('order_coordinates_from').value = routePanelControl.routePanel.state.get("from");
+                document.getElementById('order_coordinates_to').value = routePanelControl.routePanel.state.get("to")
+            }
+        });
+
+    });
+}
